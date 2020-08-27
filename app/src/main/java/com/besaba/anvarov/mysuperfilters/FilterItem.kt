318e5a8b7bcfae388695b26c02a2075999457379
RatWar <ra7@bk.ru>
2020-08-27 10:38:34 +0500

    Init

---
+++ app/src/main/java/com/besaba/anvarov/mysuperfilters/FilterItem.kt
@@ -0,0 +1,8 @@
+package com.besaba.anvarov.mysuperfilters
+
+class FilterItem(
+    val previewUrl: String? = null,
+    val imageUrl: String? = null,
+    var filterName: String? = null,
+    var filter: MySuperFilter? = null
+)
